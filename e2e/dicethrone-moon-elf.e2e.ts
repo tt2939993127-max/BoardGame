1a6c878b16c2d2c2b00b9fea90b3604268ccc750
时侍 <2974429317@qq.com>
2026-03-20 07:53:59 +0800

    test(e2e): migrate dicethrone moon-elf and poison-burn to framework

--- e2e/dicethrone-moon-elf.e2e.ts
+++ e2e/dicethrone-moon-elf.e2e.ts
@@ -9,7 +9,8 @@
  * - 状态效果：Targeted 伤害提升结算
  */
 
-import { test, expect, type Page } from '@playwright/test';
+import type { Page } from '@playwright/test';
+import { test, expect } from './framework';
 import { STATUS_IDS } from '../src/games/dicethrone/domain/ids';
 import { RESOURCE_IDS } from '../src/games/dicethrone/domain/resources';
 import { initContext } from './helpers/common';
